7e2606d626796efc713e40bec49ec2369964ecef
pan289091315 <289091315@qq.com>
2016-11-25 15:51:20 +0800

    迁移server

---
+++ social-web/src/main/java/Application.java
@@ -0,0 +1,24 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by root on 2016/11/25.
+ */
+@Controller
+@EnableAutoConfiguration
+public class Application {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
